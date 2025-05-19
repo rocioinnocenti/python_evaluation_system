47d8a6b41fc4b44b3ba1cf15abf7563171640057
rocioinnocenti <rocioinnocenti@gmail.com>
2025-05-19 17:36:15 +0200

    Evaluation system 3
    User story 3 completed for the evaluation system.
    https://rocioinnocenti.atlassian.net/browse/ES-3

--- evaluation_system/evaluation_system.py
+++ evaluation_system/evaluation_system.py
@@ -1,28 +1,3 @@
-#USER STORY 1
-#Request data
-print("Please, introduce the requested data to calculate your average grade: ")
-
-#Request the student's name
-name = input("Name: ")
-upper_name = name.upper()
-total_sum = 0
-
-#Request the 3 grades
-for i in range(3):
-    grade = float(input("Grade: "))
-    total_sum += grade
-
-#Calculate the average grade
-def calculate_average(total_sum):
-    average_grade = total_sum / 3
-    return average_grade
-average_grade = calculate_average(total_sum)
-
-#Print message
-print("Name: ", upper_name)
-print("Your average grade is: ", average_grade)
-
-#USER STORY 2
 #Determine the student's status based on their average grade
 def status(average_grade):
     if 1 <= average_grade < 5:
@@ -36,7 +11,31 @@ def status(average_grade):
     else:
         message = "Something went wrong. You must enter grades between 1 and 10."
     return message
-status = status(average_grade) 
 
-#Print status
-print("Your status is: ", status)
+#Add loop to ask 5 students
+for i in range(5):
+
+#Request data
+    print("Please, introduce the requested data to calculate your average grade: ")
+
+#Request the student's name
+    name = input(f"Name {i + 1}: ")
+    upper_name = name.upper()
+    total_sum = 0
+
+#Request the 3 grades
+    for j in range(3):
+        grade = float(input(f"Grade {j + 1}: "))
+        total_sum += grade
+
+#Calculate the average grade
+    def calculate_average(total_sum):
+        average_grade = total_sum / 3
+        return average_grade
+    average_grade = calculate_average(total_sum)
+    student_status = status(average_grade) 
+
+#Print message
+    print("Name: ", upper_name)
+    print("Your average grade is: ", average_grade)
+    print("Your status is: ", student_status)
